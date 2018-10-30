ab082def755aecbe82c5117a9adf65dac39a6eaa
AlbertCouto <aguetta12@gmail.com>
2018-10-30 17:36:14 +0100

    Login

--- rasu_fnatik/rasu_fnatik/login.cs
+++ rasu_fnatik/rasu_fnatik/login.cs
@@ -8,11 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace rasu_fnatik
 {
     public partial class login : Form
     {
+        public string connectionString = "Server=W10B304PC21\\SQLEXPRESS;Database=SecureCode;Trusted_Connection=True";
+        public SqlConnection conexion = new SqlConnection();
+        DataSet ds;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+
         public login()
         {
             InitializeComponent();
@@ -36,8 +44,15 @@ namespace rasu_fnatik
 
         private void btn_login_Click(object sender, EventArgs e)
         {
+            Abrir();
+
+            string query = "select Password from Users where Login = " + textBox1.Text;
+            da = new SqlDataAdapter(query, conexion);
+            da.Fill(ds);
+
+            Cerrar();
             //logar y chapar
-            if ((textBox2.Text).Equals("hola")) {
+            if ((textBox2.Text).Equals(ds)) {
                 frmMenu menu_metro = new frmMenu();
                 menu_metro.Show();
                 this.Close();
@@ -45,7 +60,24 @@ namespace rasu_fnatik
             else
             {
                 label3.Visible = true;
+            }            
+        }
+        private void Abrir()
+        {
+            try
+            {
+                conexion.ConnectionString = connectionString;
+                conexion.Open();
             }
-        } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al abrir la BD");
+            }
+        }
+
+        private void Cerrar()
+        {
+            conexion.Close();
+        }
     }
 }
