86368870e370a89970ad46fa37766f27e5e6ab50
AlbertCouto <aguetta12@gmail.com>
2018-11-13 17:32:41 +0100

    raton

---
+++ rasu_fnatik/Clase_bbdd_fnatik/Prueavs/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clase_bbdd_fnatik;
+
+namespace Prueavs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataSet taula = new DataSet();
+            Clase_BBDD cd = new Clase_BBDD();
+            taula = cd.PortarPerId(1, "Users");
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = taula;
+        }
+    }
+}
