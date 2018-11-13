796a7ea63f14a90ef950deb0c19c33df3a1273c1
SamuGB <43610968+SamuGB@users.noreply.github.com>
2018-11-13 17:36:10 +0100

    uj7jnyebht

---
+++ rasu_fnatik/SdsCodi_FNATIK/Sds/Sds/sdsCodi.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Sds
+{
+    partial class sdsCodi
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label = new System.Windows.Forms.Label();
+            this.TextCode = new System.Windows.Forms.TextBox();
+            this.TextDesc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(16, 13);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(35, 13);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "label1";
+            // 
+            // TextCode
+            // 
+            this.TextCode.Location = new System.Drawing.Point(57, 10);
+            this.TextCode.Name = "TextCode";
+            this.TextCode.Size = new System.Drawing.Size(100, 20);
+            this.TextCode.TabIndex = 1;
+            this.TextCode.Validating += new System.ComponentModel.CancelEventHandler(this.TextCode_Validating);
+            // 
+            // TextDesc
+            // 
+            this.TextDesc.Location = new System.Drawing.Point(163, 10);
+            this.TextDesc.Name = "TextDesc";
+            this.TextDesc.Size = new System.Drawing.Size(244, 20);
+            this.TextDesc.TabIndex = 2;
+            // 
+            // sdsCodi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TextDesc);
+            this.Controls.Add(this.TextCode);
+            this.Controls.Add(this.Label);
+            this.Name = "sdsCodi";
+            this.Size = new System.Drawing.Size(434, 41);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TextBox TextCode;
+        private System.Windows.Forms.TextBox TextDesc;
+    }
+}
---
+++ rasu_fnatik/SdsCodi_FNATIK/Sds/Sds/sdsCodi.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows.Forms;
+
+namespace Sds
+{
+    public partial class sdsCodi : UserControl
+    {
+        public sdsCodi()
+        {
+            InitializeComponent();
+        }
+
+        //PROPIEDADES
+        private bool _requerido;
+        private string _formCS;
+        private string _claseCS;
+        private string _nombreID;
+        private string _nombreCodi;
+        private string _nombreDesc;
+        private string _nombreTaula;
+        private string _controlID;
+        private string _validaCodi;
+        private string _abreCS;
+
+        public bool Requerido
+        {
+            get { return _requerido; }
+            set { _requerido = value; }
+        }
+
+        public string FormCS
+        {
+            get { return _formCS; }
+            set { _formCS = value; }
+        }
+
+        public string ClaseCS
+        {
+            get { return _claseCS; }
+            set { _claseCS = value; }
+        }
+
+        public string NombreID
+        {
+            get { return _nombreID; }
+            set { _nombreID = value; }
+        }
+
+        public string NombreCodi
+        {
+            get { return _nombreCodi; }
+            set { _nombreCodi = value; }
+        }
+
+        public string NombreDesc
+        {
+            get { return _nombreDesc; }
+            set { _nombreDesc = value; }
+        }
+
+        public string NombreTaula
+        {
+            get { return _nombreTaula; }
+            set { _nombreTaula = value; }
+        }
+
+        public string ControlID
+        {
+            get { return _controlID; }
+            set { _controlID = value; }
+        }
+
+        public string ValidaCodi
+        {
+            get { return _validaCodi; }
+            set { _validaCodi = value; }
+        }
+
+        public string AbreCS
+        {
+            get { return _abreCS; }
+            set { _abreCS = value; }
+        }
+
+
+        //EVENTOS
+        private void TextCode_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = _requerido && TextCode.Text.Trim().Equals("");
+        }
+    }
+}
