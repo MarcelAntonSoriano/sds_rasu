d9070d059c32f53d1bd78cde643aa6e91cd4d57a
MarcelAntonSoriano <marcel12772@gmail.com>
2018-12-04 18:16:09 +0100

    Revert "cambio forms"
    This reverts commit 7e50516f4dd71eef182290ffbfcbbf76f66e3e5a.

--- rasu_fnatik/gestion_categorias/Form_categorias.Designer.cs
+++ rasu_fnatik/gestion_categorias/Form_categorias.Designer.cs
@@ -33,10 +33,6 @@
             this.controlTextBox3 = new Control_FNATIK.ControlTextBox();
             this.controlTextBox2 = new Control_FNATIK.ControlTextBox();
             this.controlTextBox1 = new Control_FNATIK.ControlTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -101,52 +97,11 @@
             this.controlTextBox1.Size = new System.Drawing.Size(100, 20);
             this.controlTextBox1.TabIndex = 3;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(196, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Codigo Categoria:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(218, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Descripcion:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(196, 139);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Nivel de Acceso:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 19);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(282, 25);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "GESTIÓN DE CATEGORIAS";
-            // 
             // Form_categorias
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.controlTextBox4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.controlTextBox3);
@@ -154,17 +109,13 @@
             this.Controls.Add(this.controlTextBox1);
             this.KeyPreview = true;
             this.Name = "Form_categorias";
-            this.Text = "Gestión Categorias";
+            this.Text = "Form_categorias";
             this.Load += new System.EventHandler(this.Form_categorias_Load_1);
             this.Controls.SetChildIndex(this.controlTextBox1, 0);
             this.Controls.SetChildIndex(this.controlTextBox2, 0);
             this.Controls.SetChildIndex(this.controlTextBox3, 0);
             this.Controls.SetChildIndex(this.panel1, 0);
             this.Controls.SetChildIndex(this.controlTextBox4, 0);
-            this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.label3, 0);
-            this.Controls.SetChildIndex(this.label4, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,9 +128,5 @@
         private Control_FNATIK.ControlTextBox controlTextBox3;
         private System.Windows.Forms.Panel panel1;
         private Control_FNATIK.ControlTextBox controlTextBox4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
     }
 }
--- rasu_fnatik/gestion_rangos/Form_rangos.Designer.cs
+++ rasu_fnatik/gestion_rangos/Form_rangos.Designer.cs
@@ -32,16 +32,11 @@
             this.controlTextBox2 = new Control_FNATIK.ControlTextBox();
             this.controlTextBox1 = new Control_FNATIK.ControlTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // controlTextBox3
             // 
             this.controlTextBox3.Campo = "idUserRank";
-            this.controlTextBox3.CausesValidation = false;
-            this.controlTextBox3.ControlSds = null;
             this.controlTextBox3.EsForanea = false;
             this.controlTextBox3.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Numero;
             this.controlTextBox3.Location = new System.Drawing.Point(639, 71);
@@ -53,8 +48,6 @@
             // controlTextBox2
             // 
             this.controlTextBox2.Campo = "DescRank";
-            this.controlTextBox2.CausesValidation = false;
-            this.controlTextBox2.ControlSds = null;
             this.controlTextBox2.EsForanea = false;
             this.controlTextBox2.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox2.Location = new System.Drawing.Point(320, 111);
@@ -66,8 +59,6 @@
             // controlTextBox1
             // 
             this.controlTextBox1.Campo = "CodeRank";
-            this.controlTextBox1.CausesValidation = false;
-            this.controlTextBox1.ControlSds = null;
             this.controlTextBox1.EsForanea = false;
             this.controlTextBox1.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox1.Location = new System.Drawing.Point(320, 62);
@@ -83,56 +74,22 @@
             this.panel1.Size = new System.Drawing.Size(117, 64);
             this.panel1.TabIndex = 6;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(203, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Codigo de Rango:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(230, 111);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Descripcion:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
-            this.label3.Location = new System.Drawing.Point(12, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(236, 25);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "GESTIÓN DE RANGOS";
-            // 
             // Form_rangos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.controlTextBox3);
             this.Controls.Add(this.controlTextBox2);
             this.Controls.Add(this.controlTextBox1);
             this.Name = "Form_rangos";
-            this.Text = "Gestión de Rangos";
+            this.Text = "Form_rangos";
             this.Load += new System.EventHandler(this.Form_rangos_Load);
             this.Controls.SetChildIndex(this.controlTextBox1, 0);
             this.Controls.SetChildIndex(this.controlTextBox2, 0);
             this.Controls.SetChildIndex(this.controlTextBox3, 0);
             this.Controls.SetChildIndex(this.panel1, 0);
-            this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.label3, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,8 +101,5 @@
         private Control_FNATIK.ControlTextBox controlTextBox2;
         private Control_FNATIK.ControlTextBox controlTextBox3;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
