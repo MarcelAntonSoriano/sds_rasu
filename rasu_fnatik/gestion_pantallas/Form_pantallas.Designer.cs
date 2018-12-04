99bc38ae91f50afadc82a8aa0e587046f1e95728
MarcelAntonSoriano <marcel12772@gmail.com>
2018-12-04 19:53:07 +0100

    title

--- rasu_fnatik/gestion_pantallas/Form_pantallas.Designer.cs
+++ rasu_fnatik/gestion_pantallas/Form_pantallas.Designer.cs
@@ -235,7 +235,9 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlText;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label5.Location = new System.Drawing.Point(31, 25);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(209, 25);
