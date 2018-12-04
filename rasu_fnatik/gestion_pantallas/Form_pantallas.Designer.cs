7e50516f4dd71eef182290ffbfcbbf76f66e3e5a
Toonii <toni.arandasds@gmail.com>
2018-12-04 17:17:57 +0100

    cambio forms
    sassadadas

--- rasu_fnatik/gestion_pantallas/Form_pantallas.Designer.cs
+++ rasu_fnatik/gestion_pantallas/Form_pantallas.Designer.cs
@@ -37,11 +37,17 @@
             this.controlTextBox7 = new Control_FNATIK.ControlTextBox();
             this.controlTextBox8 = new Control_FNATIK.ControlTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // controlTextBox1
             // 
             this.controlTextBox1.Campo = "nom_dll";
+            this.controlTextBox1.CausesValidation = false;
+            this.controlTextBox1.ControlSds = null;
             this.controlTextBox1.EsForanea = false;
             this.controlTextBox1.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox1.Location = new System.Drawing.Point(162, 87);
@@ -53,6 +59,8 @@
             // controlTextBox2
             // 
             this.controlTextBox2.Campo = "nom_form";
+            this.controlTextBox2.CausesValidation = false;
+            this.controlTextBox2.ControlSds = null;
             this.controlTextBox2.EsForanea = false;
             this.controlTextBox2.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox2.Location = new System.Drawing.Point(162, 124);
@@ -64,6 +72,8 @@
             // controlTextBox3
             // 
             this.controlTextBox3.Campo = "nivel_acces";
+            this.controlTextBox3.CausesValidation = false;
+            this.controlTextBox3.ControlSds = null;
             this.controlTextBox3.EsForanea = false;
             this.controlTextBox3.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox3.Location = new System.Drawing.Point(162, 158);
@@ -75,6 +85,8 @@
             // controlTextBox4
             // 
             this.controlTextBox4.Campo = "texto";
+            this.controlTextBox4.CausesValidation = false;
+            this.controlTextBox4.ControlSds = null;
             this.controlTextBox4.EsForanea = false;
             this.controlTextBox4.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox4.Location = new System.Drawing.Point(306, 87);
@@ -86,6 +98,8 @@
             // controlTextBox5
             // 
             this.controlTextBox5.Campo = "bgcolor";
+            this.controlTextBox5.CausesValidation = false;
+            this.controlTextBox5.ControlSds = null;
             this.controlTextBox5.EsForanea = false;
             this.controlTextBox5.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox5.Location = new System.Drawing.Point(306, 124);
@@ -97,6 +111,8 @@
             // controlTextBox6
             // 
             this.controlTextBox6.Campo = "textcolor";
+            this.controlTextBox6.CausesValidation = false;
+            this.controlTextBox6.ControlSds = null;
             this.controlTextBox6.EsForanea = false;
             this.controlTextBox6.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox6.Location = new System.Drawing.Point(462, 87);
@@ -108,6 +124,8 @@
             // controlTextBox7
             // 
             this.controlTextBox7.Campo = "nom_tabla";
+            this.controlTextBox7.CausesValidation = false;
+            this.controlTextBox7.ControlSds = null;
             this.controlTextBox7.EsForanea = false;
             this.controlTextBox7.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Texto;
             this.controlTextBox7.Location = new System.Drawing.Point(462, 124);
@@ -119,6 +137,8 @@
             // controlTextBox8
             // 
             this.controlTextBox8.Campo = "id";
+            this.controlTextBox8.CausesValidation = false;
+            this.controlTextBox8.ControlSds = null;
             this.controlTextBox8.Enabled = false;
             this.controlTextBox8.EsForanea = false;
             this.controlTextBox8.EsTipo = Control_FNATIK.ControlTextBox.Tipos.Numero;
@@ -135,11 +155,52 @@
             this.panel1.Size = new System.Drawing.Size(108, 39);
             this.panel1.TabIndex = 11;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.label4.Location = new System.Drawing.Point(12, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(268, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "GESTIÓN DE PANTALLAS";
+            // 
             // Form_pantallas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.controlTextBox8);
             this.Controls.Add(this.controlTextBox7);
@@ -150,7 +211,7 @@
             this.Controls.Add(this.controlTextBox2);
             this.Controls.Add(this.controlTextBox1);
             this.Name = "Form_pantallas";
-            this.Text = "Form_pantallas";
+            this.Text = "Gestión Pantallas";
             this.Load += new System.EventHandler(this.Form_pantallas_Load);
             this.Controls.SetChildIndex(this.controlTextBox1, 0);
             this.Controls.SetChildIndex(this.controlTextBox2, 0);
@@ -161,6 +222,10 @@
             this.Controls.SetChildIndex(this.controlTextBox7, 0);
             this.Controls.SetChildIndex(this.controlTextBox8, 0);
             this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,5 +242,9 @@
         private Control_FNATIK.ControlTextBox controlTextBox7;
         private Control_FNATIK.ControlTextBox controlTextBox8;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
